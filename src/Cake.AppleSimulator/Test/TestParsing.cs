5a80a7abf45c2c5114b67e5159c491c8b802b577
Joseph Petersen <josephp90@gmail.com>
2017-08-16 19:38:21 +0200

    Pass TestCase name to lists from stdout

--- src/Cake.AppleSimulator/Test/TestParsing.cs
+++ src/Cake.AppleSimulator/Test/TestParsing.cs
@@ -9,6 +9,7 @@ namespace Cake.AppleSimulator.UnitTest
     {
         public static void TestResultsFromStdOut(IProcess process, TestResults testResults)
         {
+            char tab = '\u0009';
             foreach (var line in process.GetStandardOutput().Reverse())
             {
                 // Unit for Devices = "Tests run: 0 Passed: 0 Failed: 0 Skipped: 0"
@@ -30,7 +31,24 @@ namespace Cake.AppleSimulator.UnitTest
                         testResults.Failed = int.Parse(testArray[3]);
                         testResults.Skipped = int.Parse(testArray[4]);
                     }
-                    break;
+                }
+                else if (line.Contains(tab.ToString()))
+                {
+                    string passedTestCaseLine = line.Substring(line.IndexOf("[PASS]", StringComparison.Ordinal));
+                    string skippedTestCaseLine = line.Substring(line.IndexOf("[SKIPPED]", StringComparison.Ordinal));
+                    string failedTestCaseLine = line.Substring(line.IndexOf("[FAIL]", StringComparison.Ordinal));
+                    if (!String.IsNullOrWhiteSpace(passedTestCaseLine))
+                    {
+                        testResults.PassedList.Add(passedTestCaseLine.Trim());
+                    }
+                    else if (!String.IsNullOrWhiteSpace(skippedTestCaseLine))
+                    {
+                        testResults.SkippedList.Add(skippedTestCaseLine.Trim());
+                    }
+                    else if (!String.IsNullOrWhiteSpace(failedTestCaseLine))
+                    {
+                        testResults.FailedList.Add(failedTestCaseLine.Trim());
+                    }
                 }
             }
         }
